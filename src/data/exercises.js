a4b69c18bbe3cc2bfef8e5a1479af590c3182d77
Anthony Kizer <anthony.kizer88@gmail.com>
2021-01-15 18:25:10 -0800

    added exercises object to data folder

---
+++ src/data/exercises.js
@@ -0,0 +1,34 @@
+export const exercises = {
+  main: ["Swings", "Cleans", "Clean & Press", "Snatches", "Renegade Rows"],
+  mainData: ["swings", "cleans", "cleanPress", "snatches", "renegadeRows"],
+  assistanceLower: [
+    "Deadlift",
+    "Goblet Squat",
+    "Racked Squat",
+    "Romanian Deadlift",
+    "Stiff-Legged Deadlift",
+  ],
+  assistanceLowerData: [
+    "deadlift",
+    "gobletSquat",
+    "rackedSquat",
+    "romanianDeadlift",
+    "stiffLeggedDeadlift",
+  ],
+  assistanceUpper: [
+    "Press",
+    "Halos",
+    "Suitcase Row",
+    "Regular Rows",
+    "Bottom-up Press",
+  ],
+  assistanceUpperData: [
+    "press",
+    "halos",
+    "suitcaseRow",
+    "regularRows",
+    "bottomUpPress",
+  ],
+  conditioning: ["Tactical Lunges", "Alternating Swings", "Turkish Getup"],
+  conditioningData: ["tacticalLunges", "alternatingSwings", "turkishGetup"],
+};
